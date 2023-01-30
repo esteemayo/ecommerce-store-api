6e19a8e3344fdf5beb315235ab956711ad2ca662
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 00:51:57 +0100

    feat: add development logging

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 import 'colors';
 
 const app = express();
+
+if (app.get('env') === 'development') {
+  app.use(morgan('dev'));
+}
 
 export default app;
