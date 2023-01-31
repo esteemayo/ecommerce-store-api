ce7404d0e372ea71ae82b35d9c5019baa6bdc078
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 02:14:20 +0100

    fix: add express body parser middleware

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/users', userRoute);
 
