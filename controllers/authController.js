9a493d5902cd177a3e957b8ace5ca15dc60a3796
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 23:53:24 +0100

    fix: import AppError

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,7 @@ import BadRequestError from '../errors/badRequest.js';
 import createSendToken from '../utils/createSendToken.js';
 import createSendGoogleToken from '../utils/createSendGoogleToken.js';
 import NotFoundError from '../errors/notFound.js';
+import AppError from '../errors/AppError.js';
 
 const register = asyncHandler(async (req, res, next) => {
   const user = await User.create({ ...req.body });
@@ -50,7 +51,55 @@ const googleLogin = asyncHandler(async (req, res, next) => {
   createSendGoogleToken(newUser, StatusCodes.OK, req, res);
 });
 
-const forgotPassword = asyncHandler(async (req, res, next) => { });
+const forgotPassword = asyncHandler(async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return next(new BadRequestError('Please enter your email address'));
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(
+      new NotFoundError('There is no user user with the email address')
+    );
+  }
+
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/reset-password/${resetToken}`;
+
+  const message = `
+    Hi ${user.name},
+    There was a request to change your password!
+    If you did not make this request then please ignore this email.
+    Otherwise, please click this link to change your password: ${resetURL}
+  `;
+
+  const html = `
+    <div style='background: #f7f7f7; color: #333; padding: 50px; text-align: left;'>
+      <h3>Hi ${user.name},</h3>
+      <p>There was a request to change your password!</p>
+      <p>If you did not make this request then please ignore this email.</p>
+      <p>Otherwise, please click this link to change your password: 
+        <a href='${resetURL}'>Reset my password →</a>
+      </p>
+    </div>
+  `;
+
+  try {
+
+  } catch (err) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+
+    return next(AppError)
+  }
+});
 
 const resetPassword = asyncHandler(async (req, res, next) => { });
 
