918001aaffbf601ed38c80d362b8e780118e9b1e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-01 00:28:55 +0100

    feat: add update password auth endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@
 import express from 'express';
 
 import authController from '../controllers/authController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
@@ -14,5 +15,11 @@ router.post('/google-login', authController.googleLogin);
 router.post('/forgot-password', authController.forgotPassword);
 
 router.post('/reset-password/:token', authController.resetPassword);
+
+router.patch(
+  '/update-my-password',
+  authMiddleware.protect,
+  authController.updatePasword
+);
 
 export default router;
