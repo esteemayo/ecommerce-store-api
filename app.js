fcfa692c46d1e5bca9e6bd206c4bcf3c8ae6582f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 01:03:59 +0100

    feat: handle not found routes error

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ import express from 'express';
 import morgan from 'morgan';
 import 'colors';
 
+import NotFoundError from './errors/notFound.js';
+
 const app = express();
 
 if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
+
+app.all('*', (req, res, next) => {
+  next(new NotFoundError(`Can't find ${req.originalUrl} on this server`));
+});
 
 export default app;
