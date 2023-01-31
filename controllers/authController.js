9d8911c88c63c4df41309b1a45298d79db12abdb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 01:58:42 +0100

    feat: create register new user logic

---
+++ controllers/authController.js
@@ -0,0 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+import User from '../models/User.js';
+
+const register = asyncHandler(async (req, res, next) => {
+  const user = await User.create({ ...req.body });
+
+  if (user) {
+    return res.status(StatusCodes.CREATED).json({
+      status: 'success',
+      ...user,
+    });
+  }
+});
+
+const login = asyncHandler(async (req, res, next) => { });
+
+const forgotPassword = asyncHandler(async (req, res, next) => { });
+
+const resetPassword = asyncHandler(async (req, res, next) => { });
+
+const updatePasword = asyncHandler(async (req, res, next) => { });
+
+const authController = {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+  updatePasword,
+};
+
+export default authController;
