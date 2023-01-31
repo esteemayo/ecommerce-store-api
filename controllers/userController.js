a9d27a3f714137ad5570b02aa3e805a29aeb265e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-01 00:51:18 +0100

    fix: handle get all users logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,15 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/User.js';
 import AppError from '../errors/AppError.js';
 
-const getUsers = asyncHandler(async (req, res, next) => { });
+const getUsers = asyncHandler(async (req, res, next) => {
+  const query = req.query.new;
+
+  const users = query
+    ? await User.find().sort('-createdAt').limit(5)
+    : await User.find().sort('-_id');
+
+  res.status(StatusCodes.OK).json(users);
+});
 
 const getUserStats = asyncHandler(async (req, res, next) => { });
 
@@ -16,8 +24,24 @@ const updateUser = asyncHandler(async (req, res, next) => { });
 
 const updateMe = asyncHandler(async (req, res, next) => { });
 
+const deleteUser = asyncHandler(async (req, res, next) => { });
+
 const deleteMe = asyncHandler(async (req, res, next) => { });
 
 const createUser = (req, res, next) => { };
 
 const getMe = (req, res, next) => { };
+
+const userController = {
+  getUsers,
+  getUserStats,
+  getUser,
+  getMe,
+  createUser,
+  updateUser,
+  updateMe,
+  deleteUser,
+  deleteMe,
+};
+
+export default userController;
