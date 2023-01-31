7cfe3f996da5a2c29dc580f79c07c935344d9821
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 02:46:31 +0100

    fix: create login user logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
+import UnauthenticatedError from '../errors/unauthenticated.js';
+import BadRequestError from '../errors/badRequest.js';
 
 const register = asyncHandler(async (req, res, next) => {
   const user = await User.create({ ...req.body });
@@ -15,7 +17,24 @@ const register = asyncHandler(async (req, res, next) => {
   }
 });
 
-const login = asyncHandler(async (req, res, next) => { });
+const login = asyncHandler(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return next(new BadRequestError('Please provide username and password'));
+  }
+
+  const user = await User.findOne({ username });
+
+  if (!user || !(await user.comparePassword(password))) {
+    return next(new UnauthenticatedError('Incorrect email or password'));
+  }
+
+  return res.status(StatusCodes.OK).json({
+    status: 'success',
+    ...user._doc,
+  });
+});
 
 const forgotPassword = asyncHandler(async (req, res, next) => { });
 
