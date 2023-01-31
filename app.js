f1ae27f883c0cb29f8ff5e0ce95ed2b19d3ab51a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 01:22:00 +0100

    feat: add auth/users route

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import express from 'express';
 import morgan from 'morgan';
 import 'colors';
 
+import authRoute from './routes/auth.js';
 import NotFoundError from './errors/notFound.js';
+import userRoute from './routes/users.js';
 import globalErrorHandler from './errors/errorHandler.js';
 
 const app = express();
@@ -11,6 +13,9 @@ const app = express();
 if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/users', userRoute);
 
 app.all('*', (req, res, next) => {
   next(new NotFoundError(`Can't find ${req.originalUrl} on this server`));
