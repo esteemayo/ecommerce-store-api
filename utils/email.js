12c5bb95ff0805f730df32e75f3e6c593f55238a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 23:37:23 +0100

    feat: configure nodemailer

---
+++ utils/email.js
@@ -0,0 +1,24 @@
+import nodemailer from 'nodemailer';
+
+const sendEmail = async (options) => {
+  const smtpTransporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: `Support <${process.env.MAIL_FROM}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    html: options.html,
+  };
+
+  return await smtpTransporter.sendMail(mailOptions);
+};
+
+export default sendEmail;
