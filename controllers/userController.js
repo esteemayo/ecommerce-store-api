14fee56bc81aef66df52b945081a0d00b9be407e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-01 00:43:42 +0100

    feat: create user controller

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import _ from 'lodash';
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+import User from '../models/User.js';
+import AppError from '../errors/AppError.js';
+
+const getUsers = asyncHandler(async (req, res, next) => { });
+
+const getUserStats = asyncHandler(async (req, res, next) => { });
+
+const getUser = asyncHandler(async (req, res, next) => { });
+
+const updateUser = asyncHandler(async (req, res, next) => { });
+
+const updateMe = asyncHandler(async (req, res, next) => { });
+
+const deleteMe = asyncHandler(async (req, res, next) => { });
+
+const createUser = (req, res, next) => { };
+
+const getMe = (req, res, next) => { };
