280fa2908022bc987ccffc594eef6e0ce21f59a5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 01:12:38 +0100

    feat: add app' global error handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import 'colors';
 
 import NotFoundError from './errors/notFound.js';
+import globalErrorHandler from './errors/errorHandler.js';
 
 const app = express();
 
@@ -14,5 +15,7 @@ if (app.get('env') === 'development') {
 app.all('*', (req, res, next) => {
   next(new NotFoundError(`Can't find ${req.originalUrl} on this server`));
 });
+
+app.use(globalErrorHandler);
 
 export default app;
