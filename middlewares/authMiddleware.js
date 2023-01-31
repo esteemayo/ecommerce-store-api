cfde891e18acbd22d4f5edf4144c35f462dc75b9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 04:07:35 +0100

    feat: add user authentication middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import asyncHandler from 'express-async-handler';
+
+import User from '../models/User.js';
+import UnauthenticatedError from '../errors/unauthenticated.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    token = authHeader.split(' ')[1];
+  } else if (req.cookies.access_token) {
+    token = req.cookies.access_token;
+  }
+
+  if (!token) {
+    return next(
+      new UnauthenticatedError(
+        'You are not logged in! Please log in to get access'
+      )
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new UnauthenticatedError(
+        'The user belonging to this token does no longer exist'
+      )
+    );
+  }
+
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new UnauthenticatedError(
+        'User recently changed password! Please log in again'
+      )
+    );
+  }
+
+  req.user = currentUser;
+  next();
+});
+
+const authMiddleware = {
+  protect,
+};
+
+export default authMiddleware;
