5f50496ccf4553ee2d44466bfb1ab8d858eb739f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 02:01:00 +0100

    feat: add register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import authController from '../controllers/authController.js';
+
 const router = express.Router();
+
+router.post('/register', authController.register);
 
 export default router;
