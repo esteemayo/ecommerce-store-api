ef2606498f3013f6799b7822adb103cfb1a76cc6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-01 00:55:34 +0100

    fix: add users crud api endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,19 @@
 import express from 'express';
 
+import authMiddleware from '../middlewares/authMiddleware.js';
+import userController from '../controllers/userController.js';
+
 const router = express.Router();
+
+router
+  .route('/')
+  .get(userController.getUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 export default router;
