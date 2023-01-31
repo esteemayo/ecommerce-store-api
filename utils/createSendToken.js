ed443045bc19ccb9c7368d0719b003cc5e654f3f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 03:13:54 +0100

    fix: store token on cookies

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -1,5 +1,16 @@
 /* eslint-disable */
 const createSendToken = (user, statusCode, req, res) => {
+  const token = user.generateAuthToken();
+
+  res.cookie('access_token', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    sameSite: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
+
   const { password, role, ...rest } = user._doc;
 
   const details = {
