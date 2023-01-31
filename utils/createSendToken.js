2155385ec1e68debb9a5e73be7908333d754243b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 02:59:10 +0100

    fix: remove password from response

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -1,7 +1,14 @@
 const createSendToken = (user, statusCode, req, res) => {
+  const { password, role, ...rest } = user._doc;
+
+  const details = {
+    ...rest,
+  };
+
   res.status(statusCode).json({
     status: 'success',
-    ...user._doc,
+    details,
+    role,
   });
 };
 
