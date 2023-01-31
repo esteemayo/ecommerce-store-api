b0b573f68137677c04f6503e4fc7e90363e989da
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-31 23:48:34 +0100

    fix: create a method to generate password reset token

--- models/User.js
+++ models/User.js
@@ -102,6 +102,19 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   return false;
 };
 
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.resetPasswordExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
