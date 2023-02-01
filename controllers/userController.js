67abb8d0de5dc20119eb4086dcf98270eaebbea2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-01 01:03:05 +0100

    fix: handle get user logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
+import NotFoundError from '../errors/notFound.js';
 import AppError from '../errors/AppError.js';
 
 const getUsers = asyncHandler(async (req, res, next) => {
@@ -18,7 +19,19 @@ const getUsers = asyncHandler(async (req, res, next) => {
 
 const getUserStats = asyncHandler(async (req, res, next) => { });
 
-const getUser = asyncHandler(async (req, res, next) => { });
+const getUser = asyncHandler(async (req, res, next) => {
+  const { id: userId } = req.params;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return next(
+      new NotFoundError(`There is no user found with the given ID ↔ ${userId}`)
+    );
+  }
+
+  res.status(StatusCodes.OK).json(user);
+});
 
 const updateUser = asyncHandler(async (req, res, next) => { });
 
