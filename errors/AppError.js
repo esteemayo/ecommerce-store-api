51cd703998d9ca939e2ded0b4f2544774d28a14a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-01 03:28:09 +0100

    fix: modify

--- errors/AppError.js
+++ errors/AppError.js
@@ -4,8 +4,8 @@ class AppError extends Error {
   constructor(message) {
     super(message);
 
-    this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
     this.status = 'error';
+    this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
   }
 }
 
