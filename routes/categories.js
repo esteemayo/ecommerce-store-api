db0c5b04551974808041b6e254b74d9c7e795084
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:53:35 +0100

    fix: define all categories api endpoint

--- routes/categories.js
+++ routes/categories.js
@@ -6,4 +6,15 @@ import categoryController from '../controllers/categoryController.js';
 
 const router = express.Router();
 
+router
+  .route('/')
+  .get(categoryController.getCategories)
+  .post(categoryController.createCategory);
+
+router
+  .route('/:id')
+  .get(categoryController.getCategory)
+  .patch(categoryController.updateCategory)
+  .delete(categoryController.deleteCategory);
+
 export default router;
