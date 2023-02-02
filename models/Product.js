0b5de0de03c36fa88785c230fe26c91c49fb69a1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 21:34:05 +0100

    fix: modify

--- models/Product.js
+++ models/Product.js
@@ -3,75 +3,78 @@ import mongoose from 'mongoose';
 
 const { model, models, Schema } = mongoose;
 
-const productSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, 'A product must have a name'],
-    maxlength: [20, 'A product title must have less or equal than 20 characters'],
-    minlength: [3, 'A product title must have more or equal than 3 characters'],
-  },
-  desc: {
-    type: String,
-    trim: true,
-    required: [true, 'A product must have a description'],
-  },
-  slug: {
-    type: String,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A product must have a price'],
-  },
-  priceDiscount: {
-    type: Number,
-    validate: {
-      validator: function (val) {
-        return val < this.price;
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'A product must have a name'],
+      maxlength: [20, 'A product title must have less or equal than 20 characters'],
+      minlength: [3, 'A product title must have more or equal than 3 characters'],
+    },
+    desc: {
+      type: String,
+      trim: true,
+      required: [true, 'A product must have a description'],
+    },
+    slug: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A product must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be less than regular price',
       },
-      message: 'Discount price ({VALUE}) must be less than regular price',
+    },
+    numberInStock: {
+      type: Number,
+      required: [true, 'A product must have numberInstock'],
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    color: {
+      type: [String],
+      default: [],
+    },
+    size: {
+      type: [String],
+      default: [],
+    },
+    category: {
+      type: String,
+      required: [true, 'A product must belong to a category'],
+    },
+    tags: {
+      type: Array,
+      validate: {
+        validator: function (val) {
+          return val && val.length > 0;
+        },
+        message: 'A product must have at least one tag',
+      },
     },
   },
-  numberInStock: {
-    type: Number,
-    required: [true, 'A product must have numberInstock'],
-  },
-  inStock: {
-    type: Boolean,
-    default: true,
-  },
-  images: {
-    type: [String],
-    default: [],
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-  color: {
-    type: [String],
-    default: [],
-  },
-  size: {
-    type: [String],
-    default: [],
-  },
-  category: {
-    type: String,
-    required: [true, 'A product must belong to a category'],
-  },
-  tags: {
-    type: Array,
-    validate: {
-      validator: function (val) {
-        return val && val.length > 0;
-      },
-      message: 'A product must have at least one tag',
-    },
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 productSchema.pre('save', async function (next) {
   if (!this.isModified('name')) return next();
