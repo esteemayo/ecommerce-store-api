2f35346e643097801765ccb9c9da6e735c62d538
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 04:36:51 +0100

    fix: define products api endpoint

--- routes/products.js
+++ routes/products.js
@@ -6,4 +6,15 @@ import productController from '../controllers/productController.js';
 
 const router = express.Router();
 
+router
+  .route('/')
+  .get(productController.getProducts)
+  .post(productController.createProduct);
+
+router
+  .route('/:id')
+  .get(productController.getProductById)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
+
 export default router;
