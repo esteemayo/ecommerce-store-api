21bc8b735d690ba3ff77c6490a3dc5bbbc7e8be3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 05:03:37 +0100

    fix: add product route endpoint

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import NotFoundError from './errors/notFound.js';
 import userRoute from './routes/users.js';
 import globalErrorHandler from './errors/errorHandler.js';
 import categoryRoute from './routes/categories.js';
+import productRoute from './routes/products.js';
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/categories', categoryRoute);
+app.use('/api/v1/products', productRoute);
 
 app.all('*', (req, res, next) => {
   next(new NotFoundError(`Can't find ${req.originalUrl} on this server`));
