a7a8123b98cf50b27275b646c0ca8793c302ea3d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 02:41:13 +0100

    feat: create category model

---
+++ models/Category.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const { model, models, Schema } = mongoose;
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: [true, 'A category must have a name'],
+    maxlength: [20, 'A category title must have less or equal than 20 characters'],
+    minlength: [4, 'A category title must have more or equal than 5 characters'],
+  },
+}, {
+  timestamps: true,
+});
+
+const Category = models.Category || model('Category', categorySchema);
+
+export default Category;
