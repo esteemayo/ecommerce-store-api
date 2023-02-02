cad2b754d337b83e0627c75b9854abd07ef2b04d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 20:24:21 +0100

    feat: create order controller

---
+++ controllers/orderController.js
@@ -0,0 +1,10 @@
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+import Order from '../models/Order.js';
+import APIFeatures from '../utils/apiFeatures.js';
+import NotFoundError from '../errors/notFound.js';
+
+const orderController = {};
+
+export default orderController;
