81736a3b2b5e52532e268e54ed9fe471a42bf739
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 18:26:23 +0100

    feat: handle sortimg and other querying

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,7 +7,36 @@ import Product from '../models/Product.js';
 import NotFoundError from '../errors/notFound.js';
 
 const getProducts = asyncHandler(async (req, res, next) => {
-  const products = await Product.find();
+  const queryObj = { ...req.query };
+  const excludedFields = ['page', 'limit', 'sort', 'fields'];
+  excludedFields.forEach((item) => delete queryObj[item]);
+
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+  let query = Product.find(JSON.parse(queryStr));
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query = query.select(fields);
+  } else {
+    query = query.select('-__v');
+  }
+
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || 20;
+
+  const skip = (page - 1) * limit;
+  query = query.skip(skip).limit(limit);
+
+  const products = await query;
 
   res.status(StatusCodes.OK).json(products);
 });
