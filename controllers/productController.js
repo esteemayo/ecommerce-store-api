faa8de3bb5dc434443c6cec766285c0e55738839
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 04:24:38 +0100

    fix: modify

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,3 +3,26 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import Product from '../models/Product.js';
+
+const getProducts = asyncHandler(async (req, res, next) => { });
+
+const getProductById = asyncHandler(async (req, res, next) => { });
+
+const getProductBySlug = asyncHandler(async (req, res, next) => { });
+
+const createProduct = asyncHandler(async (req, res, next) => { });
+
+const updateProduct = asyncHandler(async (req, res, next) => { });
+
+const deleteProduct = asyncHandler(async (req, res, next) => { });
+
+const productController = {
+  getProducts,
+  getProductById,
+  getProductBySlug,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
+
+export default productController;
