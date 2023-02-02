aeeb218bddb2367a991b9b90b1328589e5e386aa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 21:34:55 +0100

    fix: modify

--- models/User.js
+++ models/User.js
@@ -5,73 +5,76 @@ import jwt from 'jsonwebtoken';
 
 const { model, models, Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Please enter your name'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide your email address'],
-    trim: true,
-    unique: true,
-    lowercase: true,
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please enter a valid email address',
-    ],
-  },
-  username: {
-    type: String,
-    unique: true,
-    lowercase: true,
-    required: [true, 'Please enter your username'],
-    match: [/^[a-zA-Z0-9]+$/, 'Username is invalid'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide your password'],
-    minlength: 8,
-    maxlength: 1024,
-    select: false,
-  },
-  confirmPassword: {
-    type: String,
-    required: [true, 'Please confirm your password'],
-    minlength: 8,
-    maxlength: 1024,
-    validate: {
-      validator: function (val) {
-        return val === this.password;
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter your name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email address'],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please enter a valid email address',
+      ],
+    },
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [true, 'Please enter your username'],
+      match: [/^[a-zA-Z0-9]+$/, 'Username is invalid'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide your password'],
+      minlength: 8,
+      maxlength: 1024,
+      select: false,
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+      minlength: 8,
+      maxlength: 1024,
+      validate: {
+        validator: function (val) {
+          return val === this.password;
+        },
+        message: 'Passwords do not match',
       },
-      message: 'Passwords do not match',
     },
-  },
-  role: {
-    type: String,
-    enum: {
-      values: ['user', 'admin'],
-      message: 'Role is either: user or admin',
+    role: {
+      type: String,
+      enum: {
+        values: ['user', 'admin'],
+        message: 'Role is either: user or admin',
+      },
+      default: 'user',
     },
-    default: 'user',
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    fromGoogle: {
+      type: Boolean,
+      default: false,
+    },
+    image: {
+      type: String,
+    },
+    passwordChangedAt: Date,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
   },
-  active: {
-    type: Boolean,
-    default: true,
-  },
-  fromGoogle: {
-    type: Boolean,
-    default: false,
-  },
-  image: {
-    type: String,
-  },
-  passwordChangedAt: Date,
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
