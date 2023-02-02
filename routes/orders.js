48bfddc7c0665dcb99f95e1dd087bf97b2fca781
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-02 20:22:50 +0100

    feat: create order router

---
+++ routes/orders.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+import authMiddleware from '../middlewares/authMiddleware.js';
+import orderController from '../controllers/orderController.js';
+
+const router = express.Router();
+
+export default router;
