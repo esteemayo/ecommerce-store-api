60c686abd90c0b5bfbf0928ca12b4dd64017b44a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-03 01:08:49 +0100

    feat: create reviews router

---
+++ routes/reviews.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+import authMiddleware from '../middlewares/authMiddleware.js';
+import reviewController from '../controllers/reviewController.js';
+
+const router = express.Router();
+
+export default router;
