ba8a4858cff532f659c6d0b720581a6694df8aac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-03 01:14:55 +0100

    feat: handle fetch all reviews logic

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,3 +3,22 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import Review from '../models/Review.js';
+import APIFeatures from '../utils/apiFeatures.js';
+
+const getReviews = asyncHandler(async (req, res, next) => {
+  const features = new APIFeatures(Review.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const reviews = await features.query();
+
+  res.status(StatusCodes.OK).json(reviews);
+});
+
+const reviewController = {
+  getReviews,
+};
+
+export default reviewController;
