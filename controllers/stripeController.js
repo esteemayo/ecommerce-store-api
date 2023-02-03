d1d9a10f7c05162c87228f4b8bab760b1e8035eb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-03 17:13:37 +0100

    feat: handle stripe checkout payment logic

---
+++ controllers/stripeController.js
@@ -0,0 +1,20 @@
+import Stripe from 'stripe';
+import { StatusCodes } from 'http-status-codes';
+
+import asyncHandler from 'express-async-handler';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const payment = asyncHandler(async (req, res, next) => {
+  const stripeRes = await stripe.charges.create({
+    source: req.body.tokenId,
+    amount: req.body.amount,
+    currency: 'usd',
+  }, {
+    apiKey: process.env.STRIPE_SECRET_KEY,
+  });
+
+  res.status(StatusCodes.OK).json(stripeRes);
+});
+
+export default payment;
