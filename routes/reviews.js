adaaf431e272fa5c92c1592f5e8b9220cba5dda5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-03 01:47:53 +0100

    fix: define all reviews api endpoints

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,4 +6,15 @@ import reviewController from '../controllers/reviewController.js';
 
 const router = express.Router();
 
+router
+  .route('/')
+  .get(reviewController.getReviews)
+  .post(reviewController.createReview);
+
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(reviewController.updateReview)
+  .delete(reviewController.deleteReview);
+
 export default router;
