73086a8d55df2d35f14b8b151b7e3d6ebd1a6d01
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-03 02:50:19 +0100

    fix: merge params

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,7 @@ import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import reviewController from '../controllers/reviewController.js';
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router.use(authMiddleware.protect);
 
