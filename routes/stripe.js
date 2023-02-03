f419f3f7ed4daffc8ca0244ecd85d0b8ef4a1330
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-03 17:18:46 +0100

    fix: add stripe payment endpoint

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,10 @@
 import express from 'express';
 
+import authMiddleware from '../middlewares/authMiddleware.js';
+import stripeController from '../controllers/stripeController.js';
+
 const router = express.Router();
+
+router.post('/payment', authMiddleware.protect, stripeController.payment);
 
 export default router;
