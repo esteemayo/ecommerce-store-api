13bd4c4bf64efe26e266e735541ff67bd2ae7e7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-04 03:32:18 +0100

    fix: define all cart api endpoints

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,19 @@
 import express from 'express';
 
+import authMiddleware from '../middlewares/authMiddleware.js';
+import cartController from '../controllers/cartController.js';
+
 const router = express.Router();
+
+router
+  .route('/')
+  .get(cartController.getCarts)
+  .post(cartController.createCart);
+
+router
+  .route('/:id')
+  .get(cartController.getCart)
+  .patch(cartController.updateCart)
+  .delete(cartController.deleteCart);
 
 export default router;
