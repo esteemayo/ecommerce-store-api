a1cc05b272355dc7b70849a2e1e773f2a27b7889
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-04 03:06:24 +0100

    fix: modify

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -5,3 +5,22 @@ import Cart from '../models/Cart.js';
 import APIFeatures from '../utils/apiFeatures.js';
 import NotFoundError from '../errors/notFound.js';
 
+const getCarts = asyncHandler(async (req, res, next) => { });
+
+const getCart = asyncHandler(async (req, res, next) => { });
+
+const createCart = asyncHandler(async (req, res, next) => { });
+
+const updateCart = asyncHandler(async (req, res, next) => { });
+
+const deleteCart = asyncHandler(async (req, res, next) => { });
+
+const cartController = {
+  getCarts,
+  getCart,
+  createCart,
+  updateCart,
+  deleteCart,
+};
+
+export default cartController;
