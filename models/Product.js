a044671a759379b9f08dbb4788ee78e09c33eb32
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-06 02:04:56 +0100

    fix: modify

--- models/Product.js
+++ models/Product.js
@@ -77,6 +77,7 @@ const productSchema = new Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
     },
   },
   {
