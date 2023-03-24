e4f5018e23299de306d51e1845857eeb66174f8c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-24 02:07:13 +0100

    fix: modify

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,6 +1,5 @@
 import Stripe from 'stripe';
 import { StatusCodes } from 'http-status-codes';
-
 import asyncHandler from 'express-async-handler';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
