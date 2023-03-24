66199787171b19fcc73509a522671ff0b93d7eef
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-24 01:47:08 +0100

    fix: modify

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import crypto from 'crypto';
-import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 import { StatusCodes } from 'http-status-codes';
 
