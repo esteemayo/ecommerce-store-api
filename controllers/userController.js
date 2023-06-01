dd59f88fc941e064a7270a5253e46104016e4902
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-01 16:06:53 +0100

    fix: modify

--- controllers/userController.js
+++ controllers/userController.js
@@ -94,7 +94,7 @@ const updateEmail = asyncHandler(async (req, res, next) => {
 
   const user = await User.findById(req.user.id).select('+password');
 
-  if (!user || !(await user.comparePassword(password))) {
+  if (!(await user.comparePassword(password))) {
     return next(new UnauthenticatedError('Your password is incorrect'));
   }
 
