f7709c56087e0bca60f5d8e70d13199c43f10668
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-18 18:20:08 +0100

    fix: format

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -5,13 +5,16 @@ import { StatusCodes } from 'http-status-codes';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const payment = asyncHandler(async (req, res, next) => {
-  const stripeRes = await stripe.charges.create({
-    source: req.body.tokenId,
-    amount: req.body.amount,
-    currency: 'usd',
-  }, {
-    apiKey: process.env.STRIPE_SECRET_KEY,
-  });
+  const stripeRes = await stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: 'usd',
+    },
+    {
+      apiKey: process.env.STRIPE_SECRET_KEY,
+    }
+  );
 
   res.status(StatusCodes.OK).json(stripeRes);
 });
