601f1298f22ff78e1a5ef2ffe282052d289dccc9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-18 18:36:34 +0100

    fix: format

--- models/Order.js
+++ models/Order.js
@@ -7,12 +7,18 @@ const orderSchema = new Schema(
     customer: {
       type: String,
       required: [true, 'An order must belong to a customer'],
-      maxLength: [60, 'An order customer\'s name must have less or equal than 60 characters'],
+      maxLength: [
+        60,
+        "An order customer's name must have less or equal than 60 characters",
+      ],
     },
     address: {
       type: String,
       required: [true, 'Please enter your address'],
-      maxLength: [200, 'Please address must have less or equal than 200 characters'],
+      maxLength: [
+        200,
+        'Please address must have less or equal than 200 characters',
+      ],
     },
     total: {
       type: Number,
