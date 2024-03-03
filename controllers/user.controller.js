94c47d503678add0deecdcb7db6b09e69ff46d84
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-03 17:55:32 +0100

    fix: modify

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,7 +10,7 @@ import UnauthenticatedError from '../errors/unauthenticated.js';
 import User from '../models/user.model.js';
 import createSendToken from '../utils/createSendToken.js';
 
-const getUsers = asyncHandler(async (req, res, next) => {
+export const getUsers = asyncHandler(async (req, res, next) => {
   const query = req.query.new;
 
   const users = query
@@ -20,13 +20,13 @@ const getUsers = asyncHandler(async (req, res, next) => {
   res.status(StatusCodes.OK).json(users);
 });
 
-const getUserStats = asyncHandler(async (req, res, next) => {
+export const getUserStats = asyncHandler(async (req, res, next) => {
   const stats = await User.getUserStats();
 
   res.status(StatusCodes.OK).json(stats);
 });
 
-const getUser = asyncHandler(async (req, res, next) => {
+export const getUser = asyncHandler(async (req, res, next) => {
   const { id: userId } = req.params;
 
   const user = await User.findById(userId);
@@ -40,7 +40,7 @@ const getUser = asyncHandler(async (req, res, next) => {
   res.status(StatusCodes.OK).json(user);
 });
 
-const updateUser = asyncHandler(async (req, res, next) => {
+export const updateUser = asyncHandler(async (req, res, next) => {
   const { id: userId } = req.params;
 
   const updatedUser = await User.findByIdAndUpdate(
@@ -61,13 +61,14 @@ const updateUser = asyncHandler(async (req, res, next) => {
   res.status(StatusCodes.OK).json(updatedUser);
 });
 
-const updateMe = asyncHandler(async (req, res, next) => {
+export const updateMe = asyncHandler(async (req, res, next) => {
   const { password, confirmPassword } = req.body;
 
   if (password || confirmPassword) {
     return next(
       new BadRequestError(
-        `This route is not for password updates. Please use update ${req.protocol
+        `This route is not for password updates. Please use update ${
+          req.protocol
         }://${req.get('host')}/api/v1/auth/update-my-password`
       )
     );
@@ -86,7 +87,7 @@ const updateMe = asyncHandler(async (req, res, next) => {
   createSendToken(updatedUser, StatusCodes.OK, req, res);
 });
 
-const updateEmail = asyncHandler(async (req, res, next) => {
+export const updateEmail = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -105,7 +106,7 @@ const updateEmail = asyncHandler(async (req, res, next) => {
   createSendToken(user, StatusCodes.OK, req, res);
 });
 
-const deleteUser = asyncHandler(async (req, res, next) => {
+export const deleteUser = asyncHandler(async (req, res, next) => {
   const { id: userId } = req.params;
 
   const user = await User.findByIdAndDelete(userId);
@@ -119,13 +120,13 @@ const deleteUser = asyncHandler(async (req, res, next) => {
   res.status(StatusCodes.NO_CONTENT).json(null);
 });
 
-const deleteMe = asyncHandler(async (req, res, next) => {
+export const deleteMe = asyncHandler(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
 
   res.status(StatusCodes.NO_CONTENT).json(null);
 });
 
-const createUser = (req, res, next) => {
+export const createUser = (req, res, next) => {
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
     status: 'fail',
     message: `This route is not defined! Please use ${req.protocol}://${req.get(
@@ -134,22 +135,7 @@ const createUser = (req, res, next) => {
   });
 };
 
-const getMe = (req, res, next) => {
+export const getMe = (req, res, next) => {
   req.params.id = req.user.id;
   next();
 };
-
-const userController = {
-  getUsers,
-  getUserStats,
-  getUser,
-  getMe,
-  createUser,
-  updateUser,
-  updateEmail,
-  updateMe,
-  deleteUser,
-  deleteMe,
-};
-
-export default userController;
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 import express from 'express';
 
-import authMiddleware from '../middlewares/auth.middleware.js';
-import userController from '../controllers/user.controller.js';
+import * as authMiddleware from '../middlewares/auth.middleware.js';
+import * as userController from '../controllers/user.controller.js';
 
 const router = express.Router();
 
